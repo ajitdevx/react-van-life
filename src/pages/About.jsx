2f983c5958b01c60ac8ea2b665bd4d370a0d7832
Ajit Ajit <ase.ajithkumar@gmail.com>
2025-02-28 19:03:09 +0530

    about page design

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,26 @@
+import bgImg from "../assets/images/about-hero.png"
+import { Link } from "react-router"
+
 export default function About() {
-    return <h2>About</h2>
+    return (
+        <section className="about-page-container">
+            <img src={bgImg} alt="about hero image" className="about-hero-image" />
+            <div className="about-page-content">
+                <h1>Don’t squeeze in a sedan when you could relax in a van.</h1>
+                <p>
+                    Our mission is to enliven your road trip with the perfect travel van rental. Our vans are recertified before each trip to ensure your travel plans can go off without a hitch.
+                    (Hitch costs extra 😉)
+
+                    Our team is full of vanlife enthusiasts who know firsthand the magic of touring the world on 4 wheels.
+                </p>
+            </div>
+            <div className="about-page-ctx">
+                <h2>Your destination is waiting.
+                    Your van is ready.</h2>
+                <Link to="/vans" className="link-button" >
+                    Explore Vans
+                </Link>
+            </div>
+        </section>
+    )
 }
