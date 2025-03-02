8b55a7fa5d336ee6a9e8a926f8507d7357fa6211
Ajit Ajit <ase.ajithkumar@gmail.com>
2025-03-02 09:56:57 +0530

    Host van design

--- src/pages/Host/Vans.jsx
+++ src/pages/Host/Vans.jsx
@@ -2,6 +2,54 @@ import "./index.css"
 
 export default function Vans() {
     return (
-        <h2>This is host Vans</h2>
+        (
+            <section>
+                <div className="host-dashboard-vans">
+                    <div>
+                        <h2 className="host-dashboard-van-title">Your listed vans</h2>
+                        <a href="">View All</a>
+                    </div>
+                    <div className="host-vans-list">
+                        <img
+                            alt=""
+                            src="https://assets.scrimba.com/advanced-react/react-router/reliable-red.png"
+                        />
+                        <div>
+                            <h2>Modest Explorer</h2>
+                            <p className="van-price">
+                                <span>$16</span>/day
+                            </p>
+                        </div>
+                        <a href="">Edit</a>
+                    </div>
+                    <div className="host-vans-list">
+                        <img
+                            alt=""
+                            src="https://assets.scrimba.com/advanced-react/react-router/reliable-red.png"
+                        />
+                        <div>
+                            <h2>Modest Explorer</h2>
+                            <p className="van-price">
+                                <span>$16</span>/day
+                            </p>
+                        </div>
+                        <a href="">Edit</a>
+                    </div>
+                    <div className="host-vans-list">
+                        <img
+                            alt=""
+                            src="https://assets.scrimba.com/advanced-react/react-router/reliable-red.png"
+                        />
+                        <div>
+                            <h2>Modest Explorer</h2>
+                            <p className="van-price">
+                                <span>$16</span>/day
+                            </p>
+                        </div>
+                        <a href="">Edit</a>
+                    </div>
+                </div>
+            </section>
+        )
     )
 }
