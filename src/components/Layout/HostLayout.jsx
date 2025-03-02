26c6465a1249de0d6c030f8d294b703df3a73ecc
Ajit Ajit <ase.ajithkumar@gmail.com>
2025-03-02 09:55:22 +0530

    Child nav with active style

--- src/components/Layout/HostLayout.jsx
+++ src/components/Layout/HostLayout.jsx
@@ -1,14 +1,25 @@
-import { Outlet, Link } from "react-router"
+import { Outlet, NavLink } from "react-router"
 
 export default function HostLayout() {
+
+    const activeStyle = {
+        color: '#161616',
+        fontWeight: 'bold',
+        textDecoration: 'underline'
+    }
+
+    const handleActiveStyle = ({ isActive }) => {
+        return isActive ? activeStyle : null
+    }
+
     return (
         <>
             <section>
                 <nav className="host-nav">
                     <ul>
-                        <li><Link to="/host" className="nav-link">Dashboard</Link></li>
-                        <li><Link to="/host/income" className="nav-link">Income</Link></li>
-                        <li><Link to="/host/vans" className="nav-link">Vans</Link></li>
+                        <li><NavLink end style={handleActiveStyle} to="/host" className="nav-link">Dashboard</NavLink></li>
+                        <li><NavLink end style={handleActiveStyle} to="/host/income" className="nav-link">Income</NavLink></li>
+                        <li><NavLink end style={handleActiveStyle} to="/host/vans" className="nav-link">Vans</NavLink></li>
                     </ul>
                 </nav>
             </section>
